fc5de4daf4611e6295672f1142f93f3ee27e8ccd
1MuRaD12 <tu71tfsws@code.edu.az>
2022-03-03 23:46:00 +0300

    c#

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+
+
+            if (number == 1)
+            {
+                Console.WriteLine("ne tek ededdir ne de cüt");
+            }
+            else if (number % 2 == 0)
+            {
+                Console.WriteLine("cüt ededdir");
+            }
+            else if (number % 2 == 1)
+            {
+                Console.WriteLine("tek ededdir");
+                    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5, j = 8;
+
+            if (i > j)
+            {
+                Console.WriteLine("i boyukdur j den");
+            }
+            else if (i < j)
+            {
+                Console.WriteLine("i kicikdir j den");
+            }
+
+            else
+            {
+                Console.WriteLine("i beraberdi j ye");
+            }
+        }
+    }
+}
